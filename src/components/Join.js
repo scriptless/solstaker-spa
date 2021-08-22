183caf33d7c8f81f811221786945071f57e6f3ca
Alexander Kuerfgen <github@scriptless.io>
2021-08-22 20:57:49 +0400

    Fix 404 error on image

--- src/components/Join.js
+++ src/components/Join.js
@@ -4,7 +4,7 @@ class Features extends Component {
   render() {
     return <section class="px-7 py-10 rounded-xl border border-1 border-gray-700 mb-10 bg-gradient-to-tr from-gray-800 to-indigo-900">
         <div class="flex flex-col items-center">
-            <img class="h-20 w-20 mt-3 mb-5" src="../logos/exchange-white.svg" alt=""/>
+            <img class="h-20 w-20 mt-3 mb-5" src="/logos/exchange-white.svg" alt=""/>
             <h1 class="text-white text-2xl font-semibold leading-tight">Solana</h1>
             <p class="text-gray-200">APY ~8%</p>
             <span class="my-2 inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-green-100 text-green-800">
